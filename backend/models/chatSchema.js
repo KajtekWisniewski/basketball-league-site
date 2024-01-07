d15f7d1461b3f9229e78329a3a82ce1d40e81890
LePerryH <k.wisniewski.603@studms.ug.edu.pl>
2024-01-07 17:12:29 +0100

    added chat schema for db

---
+++ backend/models/chatSchema.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const chatSchema = new mongoose.Schema({
+    team_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Team',
+      required: true
+    },
+    participants: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }],
+    messages: [{
+      sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+      },
+      body: {
+        type: String,
+        required: true
+      },
+      timestamp: {
+        type: Date,
+        default: Date.now
+      }
+    }]
+  });
+
+const Chat = mongoose.model('Chat', chatSchema);
+
+module.exports = Chat;
