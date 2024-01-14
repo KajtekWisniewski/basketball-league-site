7238028aca9f760e38b9ae23f5a80f24ffad51e6
LECFanboy <64749090+LECFanboy@users.noreply.github.com>
2024-01-14 23:24:30 +0100

    fixed typo in schema ....

--- backend/models/matchSchema.js
+++ backend/models/matchSchema.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const commentSchema = new Schema({
+const commentSchema = new mongoose.Schema({
   author: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
