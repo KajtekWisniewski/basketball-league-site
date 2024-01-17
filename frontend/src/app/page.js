9a02e338efd18a3c48156fd14ee402b79d0f648c
LECFanboy <64749090+LECFanboy@users.noreply.github.com>
2024-01-17 19:43:29 +0100

    introducing redux into the app. Commit before moving from pagesrouter to app router

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,15 +1,25 @@
-import styles from './page.module.css'
-import NavBar from '../components/NavBar'
+'use client';
+import styles from './page.module.css';
+import NavBar from '../components/NavBar';
 import dynamic from 'next/dynamic';
+import { useSelector } from 'react-redux';
+import Standings from '@/components/teamComponents/Standings';
+import LogIn from '@/components/userComponents/login-page';
 
-const Standings = dynamic(() => import('../components/teamComponents/Standings'), { ssr: false });
+// const Standings = dynamic(() => import('../components/teamComponents/Standings'), {
+//   ssr: false
+// });
 
 export default function Home() {
- 
+  const username = useSelector((state) => state.authReducer.value.username);
+  const isModerator = useSelector((state) => state.authReducer.value.isModerator);
   return (
     <>
-    <NavBar></NavBar>
-    <Standings></Standings>
+      <NavBar></NavBar>
+      <LogIn></LogIn>
+      <h1>Username: {username}</h1>
+      {isModerator && <h1> this user is a moderator</h1>}
+      <Standings></Standings>
     </>
   );
-};
+}
--- frontend/src/pages/teams/teamsList.js
+++ frontend/src/pages/teams/teamsList.js
@@ -1,51 +1,58 @@
+'use client';
 import axios from 'axios';
 import TeamPreview from '../../components/teamComponents/TeamPreview';
 import React, { useState, useEffect } from 'react';
-import styles from './teamsList.module.css'
-import Link from 'next/link'
+import styles from './teamsList.module.css';
+import Link from 'next/link';
 import NavBar from '../../components/NavBar';
 import globalStyles from '../../app/globals.css';
+import { useSelector } from 'react-redux';
 
 export default function TeamsList() {
-    const [teams, setTeams] = useState(null);
-    const [sortField, setSortField] = useState('name');
-    const [sortOrder, setSortOrder] = useState('asc')
+  const [teams, setTeams] = useState(null);
+  const [sortField, setSortField] = useState('name');
+  const [sortOrder, setSortOrder] = useState('asc');
+  const username = useSelector((state) => state.authReducer.value.username);
 
-    useEffect(() => {
-      const fetchTeamList = () => {
-        axios
+  useEffect(() => {
+    const fetchTeamList = () => {
+      axios
         .get(`http://127.0.0.1:3001/teams`)
         .then((response) => {
-          setTeams(response.data)
+          setTeams(response.data);
         })
         .catch((error) => console.error('Error fetching players data:', error));
-      };
-      fetchTeamList();
-    }, [])
-
-    //placeholder for loading
-    if (!teams) {
-      return <div></div>;
-    }
-
-    const handleSort = (field) => {
-      setSortOrder((prevOrder) => (sortField === field ? (prevOrder === 'asc' ? 'desc' : 'asc') : 'asc'));
-      setSortField(field);
     };
+    fetchTeamList();
+  }, []);
 
-    //to change, sorting players by lastname instead of first
-    const sortedTeams = [...teams].sort((a, b) => {
-      const compareValue = (field) => (a[field] > b[field] ? 1 : a[field] < b[field] ? -1 : 0);
-      return sortOrder === 'asc' ? compareValue(sortField) : compareValue(sortField) * -1;
-    });
+  //placeholder for loading
+  if (!teams) {
+    return <div></div>;
+  }
 
-    return (
-      <>
+  const handleSort = (field) => {
+    setSortOrder((prevOrder) =>
+      sortField === field ? (prevOrder === 'asc' ? 'desc' : 'asc') : 'asc'
+    );
+    setSortField(field);
+  };
+
+  //to change, sorting players by lastname instead of first
+  const sortedTeams = [...teams].sort((a, b) => {
+    const compareValue = (field) =>
+      a[field] > b[field] ? 1 : a[field] < b[field] ? -1 : 0;
+    return sortOrder === 'asc' ? compareValue(sortField) : compareValue(sortField) * -1;
+  });
+
+  return (
+    <>
       <NavBar></NavBar>
+      <h1>username: {username}</h1>
       <Link className={globalStyles.linkStyle} href={`/teams/add-team`}>
-            <h1>ADD A TEAM</h1>
+        <h1>ADD A TEAM</h1>
       </Link>
-     <table className={styles.teamsTable}>
+      <table className={styles.teamsTable}>
         <thead>
           <tr>
             <th onClick={() => handleSort('name')}>Name</th>
@@ -56,10 +63,10 @@ export default function TeamsList() {
         </thead>
         <tbody>
           {sortedTeams.map((team) => (
-              <TeamPreview key={team._id} teamId={team._id} />
+            <TeamPreview key={team._id} teamId={team._id} />
           ))}
         </tbody>
       </table>
-      </>
-    ) 
-  }
+    </>
+  );
+}
