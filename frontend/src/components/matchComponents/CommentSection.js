858ac70ede3c3db6ce8090c3c9c0ffa23aecfd6f
LECFanboy <64749090+LECFanboy@users.noreply.github.com>
2024-01-19 18:07:19 +0100

    small styling to comments

--- frontend/src/components/matchComponents/CommentSection.js
+++ frontend/src/components/matchComponents/CommentSection.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import { useSelector } from 'react-redux';
+import styles from './MatchCard.module.css';
 
 const CommentSection = ({ matchId }) => {
   const [socket, setSocket] = useState(null);
@@ -76,10 +77,9 @@ const CommentSection = ({ matchId }) => {
 const CommentList = ({ comments }) => (
   <ul>
     {comments.map((comment) => (
-      <li key={comment._id}>
-        {comment.date.toString().slice(0, 19).replace('T', ' ')}
-        {comment.author?.name}
-        {comment.body}
+      <li key={comment._id} className={styles.comment}>
+        {comment.date.toString().slice(0, 19).replace('T', ' ')} -{' '}
+        {comment.author?.name} - {comment.body}
       </li>
     ))}
   </ul>
